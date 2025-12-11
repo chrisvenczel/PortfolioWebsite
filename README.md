1ecfa2b27474cfc4bb0e3251b048626edeccfe71
chrisvenczel <christopher.venczel@outlook.com>
2025-12-11 13:15:08 -0500

    Updates

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-# PortfolioWebsite
+# Portfolio Website
 
-This is the code for my interactive portfolio website. It includes the HTML, CSS, PHP etc as well as the projects themselves written in P5JS.
+This is the code for my personal portfolio website. It is built using static HTML files generated from a dynamic PHP-based site.
+
+It is deployed for free using [Netlify](https://www.netlify.com/), which serves the static files.
+
+## Licence
+
+This work is open source and free to use. Feel free to learn from, remix, or build upon it. See the [LICENSE](LICENSE) file for details.
 
 ## Prerequisites
 
-- ` sudo apt-get install httrack`
-- PHP 8.0+ CLI (for the built-in web server)
+- [HTTrack](https://www.httrack.com/) is used for copying the website to static HTML files.
+  - Install: `sudo apt-get install httrack`
+- PHP 8.0+ CLI is used to serve the website locally.
+  - Install: `sudo apt-get install php8.0-cli`
 - Node 18+ (for npm scripts)
--- src/about.php
+++ src/about.php
@@ -18,12 +18,12 @@
     <div id="centerDiv">
         <div id="mePic"></div>
         <p id="p1">
-        &emsp;Hi, I'm Christopher Venczel, a recent Computer Science graduate from the University of Ottawa. I'm passionate about web app development and enjoy exploring data visualization and analysis. This website is a portfolio of some of the interactive Javascript projects I have created using the <a class="basicLink" target="_blank" href="https://p5js.org/">P5JS</a> library.
+        &emsp;Hello, and welcome to my portfolio site! I'm Christopher Venczel, a programmer and web developer interested in data visualization and analysis. This website is a portfolio of some of the interactive Javascript projects I have created using the <a class="basicLink" target="_blank" href="https://p5js.org/">P5JS</a> library.
         </p>
         
         <p id="p2">
         &emsp;The projects feature a variety of different things that interest me including Music 🎵, Design 🎨, Math 📈, Physics 🚀, and AI 🧠.
-        The code for the projects as well as for the website is open-source under the GNU General Public Licence and can be found on my Github 
+        The code for the projects as well as for the website is open-source under the <a href="https://opensource.org/license/mit" class="basicLink" target="_blank">MIT Licence</a> and can be found on my GitHub 
         <a target="_blank" class="basicLink" href="https://github.com/chrisvenczel/PortfolioWebsite">here</a>.
         </p>
     </div>
